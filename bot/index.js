62cf84e5684d43f93fe34e312a78a1841a1d3c5f
Brix9578 <brendan95140@gmail.com>
2026-02-07 23:59:33 +0100

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -3,12 +3,21 @@ const app = express();
 
 app.use(express.json());
 
-// juste pour tester l’URL
+// test simple
 app.get("/", (req, res) => {
   res.send("Bot Black Veil Agency en ligne");
+});
+
+// 🔔 réception d’un contrat depuis le site
+app.post("/new-contract", (req, res) => {
+  console.log("📩 Nouveau contrat reçu :", req.body);
+
+  res.json({ status: "ok" });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Serveur actif sur le port " + PORT);
 });
+
+
