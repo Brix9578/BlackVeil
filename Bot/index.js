38f04f128aa9990c67f29515e83f5ea9f83b1ac7
Brix9578 <brendan95140@gmail.com>
2026-02-10 12:27:39 +0100

    Update index.js

--- Bot/index.js
+++ Bot/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // ✅ HEADERS CORS MANUELS (OBLIGATOIRE)
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://brix9578.github.io");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
